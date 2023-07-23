af06960fd17727ef89686574367fcd78f740a8bc
md ali reza <muhammd.ali@brainstation-23.com>
2023-07-23 10:39:21 +0600

    Initial Commit

---
+++ SwiftUIOtpVerificationUI/SwiftUIOtpVerificationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOtpVerificationUIApp.swift
+//  SwiftUIOtpVerificationUI
+//
+//  Created by BS1096 on 23/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOtpVerificationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
